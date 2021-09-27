e68fb71c38c73f2444694a8e25425c373388aac2
shotgunck <shotgunck137@gmail.com>
2021-09-27 10:44:25 +0700

    achievement got

--- modules/minecraft.js
+++ modules/minecraft.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
 const axios = require('axios')
 
+const config = require('../config.json')
+
 const commands = {
     mcskin: (message, arg2) => {
         axios.get('https://www.mc-heads.net/body/'+arg2+'/270')
@@ -11,7 +13,18 @@ const commands = {
             .setImage(data.config.url)
             })
         })
-    }
+    },
+
+    achieve: message => {
+        const args = message.content.slice(config.prefix.length+8).trim().split(/ +/g).join('..')
+        axios.get('https://minecraft-api.com/api/achivements/cooked_salmon/achievement..got/'+args)
+        .then(data => {
+            message.channel.send({ embed: new Discord.MessageEmbed() 
+            .setColor('#00DFFF')
+            .setImage(data.config.url)
+            })
+        })
+    },
 }
 
 module.exports = commands
