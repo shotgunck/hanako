6f2620c74c745748eaf643be48a6f762a92a3694
shotgunck <shotgunck137@gmail.com>
2021-09-27 10:20:06 +0700

    skin

--- modules/minecraft.js
+++ modules/minecraft.js
@@ -1,17 +1,14 @@
 const Discord = require('discord.js')
 const axios = require('axios')
 
-let url = 'https://minecraft-api.com/api/skins/notsoclassy/body/10/10/10/25/12/json'
-
 const commands = {
     mcskin: (message, arg2) => {
-        axios.get('https://minecraft-api.com/api/skins/'+arg2+'/body/10/10/10/25/12/json')
+        axios.get('https://www.mc-heads.net/body/'+arg2+'/270')
         .then(data => {
-            console.log(data)
             message.channel.send({ embed: new Discord.MessageEmbed() 
             .setColor('#00DFFF')
             .setTitle(arg2)
-            .setThumbnail(data)
+            .setImage(data.config.url)
             })
         })
     }
