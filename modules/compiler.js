6fbab6e13e9e963b5dfc57fe8a0810e48f90ac8a
shotgun <87556240+shotgunck@users.noreply.github.com>
2021-10-06 03:34:48 +0000

    revert

--- modules/compiler.js
+++ modules/compiler.js
@@ -1,35 +1,29 @@
-// Not yet implemented
+// Some
+const request = require('request')
 
-const { c, cpp, node, python, java } = require('compile-run')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const commands = {
     compile: async (message, arg2) => {
-       if (arg2.startsWith('```js') || arg2.startsWith('```javascript') ) {
-         node.runSource(message.content.replace(/```/g, '').replace(/^.+\n/, ''))
-         .then(res => {
-           message.channel.send('**Output:** \n\n`'+res.stdout+'`')
-         })
-       } else if (arg2.startsWith('```java')) {
-         java.runSource(message.content.replace(/```/g, '').replace(/^.+\n/, ''))
-         .then(res => {
-           message.channel.send('**Output:** \n\n`'+res.stdout+'`')
-         })
-       } else if (arg2.startsWith('```c')) {
-         c.runSource(message.cjsontent.replace(/```/g, '').replace(/^.+\n/, ''))
-         .then(res => {
-           message.channel.send('**Output:** \n\n`'+res.stdout+'`')
-         })
-       } else if (arg2.startsWith('```cpp') || arg2.startsWith('```c++')) {
-         cpp.runSource(message.content.replace(/```/g, '').replace(/^.+\n/, ''))
-         .then(res => {
-           message.channel.send('**Output:** \n\n`'+res.stdout+'`')
-         })
-       } else if (arg2.startsWith('```py') || arg2.startsWith('```python') ) {
-         python.runSource(message.content.replace(/```/g, '').replace(/^.+\n/, ''))
-         .then(res => {
-           message.channel.send('**Output:** \n\n`'+res.stdout+'`')
-         })
-       }
+        let args = message.content.substring(2).split(" ");
+        var program = {
+            script : message.content.replace(/```/g, '').replace(/^.+\n/, ''),
+            language: message.content.replace(/```/g, ''),
+            versionIndex: "2",
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET
+        }
+        request({
+            url: 'https://api.jdoodle.com/v1/execute',
+            method: "POST",
+            json: program
+        },
+        function (error, response, body) {
+            console.log('error:', error)
+            console.log('statusCode:', response && response.statusCode)
+            console.log('body:', body)
+        })
     }
 }
 
