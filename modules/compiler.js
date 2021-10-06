0ddab0f0c3946bc0c3b0fb111c5dc6e0e5b4aa35
shotgun <87556240+shotgunck@users.noreply.github.com>
2021-10-06 07:26:15 +0000

    basic compiling

--- modules/compiler.js
+++ modules/compiler.js
@@ -1,119 +1,47 @@
 // Not yet implemented
 const Discord = require('discord.js')
 const request = require('request')
-const hastebin = require('hastebin-gen')
-const htmlParser = require('html-to-json')
 
 const dotenv = require('dotenv')
 dotenv.config()
 
 const config = require('../config.json')
 
+
+
 const commands = {
     compile: async (message, arg2) => {
-      console.log('source: ', arg2.replace(/```/g, '').replace(/^.+\n/, ''))
-      console.log('lang: ', arg2.match(/\b\w+\b/g))
-
-            var program = {
-              script: arg2.replace(/```/g, '').replace(/^.+\n/, ''),
-              language: arg2.match(/\b\w+\b/g)[0],
-              versionIndex: "0",
-              clientId: process.env.CLIENT_ID,
-              clientSecret: process.env.CLIENT_SECRET
-            }
-            request
-              .post({
-                url: "https://api.jdoodle.com/execute",
-                json: program,
-              })
-              .on("error", (error) => {
-                console.log("request.post error", error);
-                return;
-              })
-              .on("data", (data) => {
-            const parsedData = JSON.parse(data.toString());
-            if (parsedData.error) {
-              console.log(parsedData.error)
-              return;
-            } else {
-              var output = "";
-              for (var i = 0; i < parsedData.output.length; i++) {
-                if (parsedData.output[i] == "\n") output += "\n";
-                else output += parsedData.output[i];
-              }
-              let outputOfProgram = new Discord.MessageEmbed()
-                .setDescription("Result!!!")
-                .setColor("33FFB3")
-                .setDescription(output);
-              message.channel.send(outputOfProgram);
-              return;
-            }
-          })
-    },
-    cbt: async (message) => {
-try {
-      let args = message.content.substring(config.prefix.length).split(" ");
-      switch (args[2]) {
-        case "run":
-      var language, index;
-      if (args[3] == "C" || args[3] == "c") {
-        language = "c";
-        index = "0";
-      } else if (args[3] == "python") {
-        language = "python3";
-        index = "3";
+      const program = {
+        script: arg2.replace(/```/g, '').replace(/^.+\n/, ''),
+        language: arg2.match(/\b\w+\b/g)[0],
+        versionIndex: "0",
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
       }
-      var fileName = message.attachments.array()[0];
-      request.get(fileName.url, (err, response, body) => {
-        console.log(body)
-        const runRequestBody = {
-          script: body,
-          language: language,
-          //stdin: input,
-          versionIndex: index,
-          clientId: process.env.CLIENT_ID,
-          clientSecret: process.env.CLIENT_SECRET,
-        };
-        request
-          .post({
-            url: "https://api.jdoodle.com/execute",
-            json: runRequestBody,
-          })
-          .on("error", (error) => {
-            console.log("request.post error", error);
-            return;
-          })
-          .on("data", (data) => {
-            console.log(data)
-            const parsedData = JSON.parse(data.toString());
-            if (parsedData.error) {
-              return;
-            } else {
-              var output = "";
-              for (var i = 0; i < parsedData.output.length; i++) {
-                if (parsedData.output[i] == "\n") output += "\n";
-                else output += parsedData.output[i];
-              }
-              let outputOfProgram = new Discord.MessageEmbed()
-                .setDescription("Result!!!")
-                .setColor("33FFB3")
-                .setDescription(output);
-              message.channel.send(outputOfProgram);
-              return;
-            }
-          });
-      });
-      break;
-    }
-}
-catch (Exception) {
-    console.log(Exception);
-    let Errorbotembed = new Discord.MessageEmbed()
-      .setDescription("Error!!!")
-      .setColor("FFFFFF")
-      .setDescription("Error Encountered\nFor Help use\nDM help");
-    message.channel.send(Errorbotembed);
-  }
+      request.post({
+        url: "https://api.jdoodle.com/execute",
+        json: program,
+      })
+      .on("error", err => {
+        message.channel.send('Executing error encountered:', error)
+      })
+      .on("data", data => {
+        const parsedData = JSON.parse(data.toString())
+        if (parsedData.error) {
+          message.channel.send('Parsing error encountered:', parsedData.error)
+        } else {
+          let output = ''
+          for (var i = 0; i < parsedData.output.length; i++) {
+            if (parsedData.output[i] == "\n") output += "\n"
+            else output += parsedData.output[i]
+          }
+          message.channel.send(new Discord.MessageEmbed()
+            .setTitle("**__Output:__**")
+            .setColor("33FFB3")
+            .setDescription(output)
+          )
+        }
+      })
     }
 }
 
