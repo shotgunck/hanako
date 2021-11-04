aec26ac83e286f5e504f51861b70cc2e7f4afad0
shotgun <87556240+shotgunck@users.noreply.github.com>
2021-11-04 07:39:39 +0000

    better lyrics filter

--- modules/music.js
+++ modules/music.js
@@ -18,8 +18,8 @@ module.exports = {
 
       queue.songs.map((song, _) => {
         let data = song.name.split(' - ')
-        const songName = !data[1]? data[0] : data[1]
-        axios.get('https://api.jastinch.xyz/lyrics/?song='+songName.replace(/\([^)]*\)/gm, ''))
+        const songName = (!data[1]? data[0] : data[1]).replace(/\([^)]*\)/gm, '')
+        axios.get('https://api.jastinch.xyz/lyrics/?song='+songName)
         .then(res => {
           message.channel.send('Lyrics for sound: **'+songName+'**\n'+res.data.lyrics+'\n--------------------------------', {split: true})
         })
